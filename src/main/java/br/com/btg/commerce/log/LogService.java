584778fa5afe9b6f9fca30aad7103bf41a9b956c
Ricardo Nascimento <ricardonascimentocontato@gmail.com>
2024-01-04 01:38:20 -0300

    Improving log service.

--- src/main/java/br/com/btg/commerce/log/LogService.java
+++ src/main/java/br/com/btg/commerce/log/LogService.java
@@ -1,21 +1,32 @@
 package br.com.btg.commerce.log;
 
-import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 @Aspect
+@Component
 public class LogService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(LogService.class);
 
-    @Pointcut("execution(* br.com.btg.commerce.service..*(..))")
-    public void logMethod(final JoinPoint joinPoint) {
+    @Around("execution(* br.com.btg.commerce.service..*(..))")
+    public Object logMethod(ProceedingJoinPoint joinPoint) throws Throwable {
+        long startTime = System.currentTimeMillis();
+
+        Object result = joinPoint.proceed();
+
+        long endTime = System.currentTimeMillis();
+        long executionTime = endTime - startTime;
+
         String methodName = joinPoint.getSignature().getName();
         String className = joinPoint.getTarget().getClass().getName();
-        LogService.LOGGER.info("Executing method {} in class {}", methodName, className);
+        LogService.LOGGER.info("Method {} in class {} executed in {} ms", methodName, className, executionTime);
+
+        return result;
     }
 
 }
